2f74deeefeb93338b81c13940c99f3c51e5068e4
Jack <jackkleyk@gmail.com>
2026-07-28 14:30:24 +1000

    Remove sensitive Information.h from tracking

--- iot/src/sensitiveInformation.h
+++
@@ -1,9 +0,0 @@
-/*
- * Contains sensitive network parameters.
- * Note: Update these credentials if connecting to a home network or hot-spot.
- * Keep this file listed in your .gitignore to avoid pushing secrets to GitHub.
- */
-
-// Wi-Fi Credentials
-const char* ssid = "SSID here";       // Network SSID
-const char* password = "Password here";  // Network Password
